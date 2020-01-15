db2fc69bc8b52bbacc717c7a8f589af3e4eadd6f
7950 <marciow.ferreira@bancoamazonia.com.br>
2020-01-15 15:47:05 -0300

    Projeto para start de outros projetos. Já possui diversas dependencias inclusas. Olhar o arquivo pom para escolher/definir as dependencias necessárias ao projeto.

---
+++ src/main/java/dev/bearded/blankprojectwithdependencies/BlankProjectWithDependenciesApplication.java
@@ -0,0 +1,13 @@
+package dev.bearded.blankprojectwithdependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankProjectWithDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlankProjectWithDependenciesApplication.class, args);
+    }
+
+}
